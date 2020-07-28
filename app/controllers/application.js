1b9fa986f9970653b8e57ab778426cae93afb666
Serg Chernata <sergchernata@gmail.com>
2020-07-28 10:01:02 -0400

    added content to product pages

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -1,6 +1,14 @@
 import Controller from '@ember/controller';
+import { inject as service } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
+    router: service(),
+    showQuoteForm: false,
+
+    currentRouteName: computed('router.currentRouteName', function() {
+        return this.get('router.currentRouteName');
+    }),
     actions: {
         scrollTo(id) {
             const element = document.getElementById(id)
