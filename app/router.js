d705e35d8835d449659ebf8da0a680d321e4386c
Serg Chernata <sergchernata@gmail.com>
2020-08-22 17:25:33 -0400

    added privacy policy, removed about us

--- app/router.js
+++ app/router.js
@@ -1,4 +1,3 @@
-import EmberRouter from '@ember/routing/router';
 import config from './config/environment';
 import EmberRouterScroll from 'ember-router-scroll';
 
@@ -13,5 +12,5 @@ Router.map(function() {
     this.route('led-verlichting');
     this.route('dakhuur');
   });
-  this.route('over-ons');
+  this.route('privacyverklaring');
 });
