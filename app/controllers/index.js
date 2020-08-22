e76e786defed33f4d4b1ba0ce1a47a56eb5a1e3f
Serg Chernata <sergchernata@gmail.com>
2020-08-22 17:24:58 -0400

    adjusted text

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,62 +3,50 @@ import Controller from '@ember/controller';
 const slides = [
     {
         title: 'Minder CO₂-uitstoot',
-        text: 'Doordat u kiest voor duurzame energie draagt u bij aan de vermindering van CO₂-uitstoot.',
+        text: 'Doordat u kiest voor duurzame oplossingen draagt u bij aan de vermindering van CO₂-uitstoot.',
         image: 'c02-icon.svg',
         alt: 'icon',
     },
     {
         title: 'Verbetering energielabel',
-        text: 'Uw energielabel wordt verbeterd door de plaatsing van zonnepanelen op uw dak.',
+        text: 'Uw energielabel wordt verbeterd door het verduurzamingsproces.',
         image: 'chart-icon.svg',
         alt: 'icon',
     },
     {
-        title: '100% groene stroom',
-        text: 'U kunt ervoor kiezen om over te gaan naar 100% groene energie verbruik.',
-        image: 'eco-icon.svg',
-        alt: 'icon',
-    },
-    {
         title: 'Invulling MVO-beleid',
-        text: 'Direct invulling aan uw MVO-beleid en u investeert aan de toekomstbestendigheid van uw bedrijf.',
+        text: 'Direct invulling aan uw MVO-beleid en u investeert in de toekomstbestendigheid van uw bedrijf.',
         image: 'mvo-icon.svg',
         alt: 'icon',
     },
     {
-        title: 'Behoud kredietruimte',
-        text: 'Wij doen de investering en u kan zich blijven richten op uw core business.',
-        image: 'bank-icon.svg',
-        alt: 'icon',
-    },
-    {
         title: 'Imagoverbetering',
-        text: 'Een positieve bijdrage aan de groene uitstraling van uw bedrijf. Zo laat u duidelijk zien dat u bijdraagt aan een beter milieu.',
+        text: 'Een positieve bijdrage aan de uitstraling van uw bedrijf. Zo laat u duidelijk zien dat u bijdraagt aan een betere wereld.',
         image: 'eye-icon.svg',
         alt: 'icon',
     },
     {
+        title: 'Besparen',
+        text: 'Door te kiezen voor duurzame oplossingen gaat u energie en geld besparen. In het geval van dak verhuur verdient u zelfs extra geld zonder investering.',
+        image: 'bank-icon.svg',
+        alt: 'icon',
+    },
+    {
         title: 'Goed geregeld',
-        text: 'Wij nemen niet alleen het werk uit handen maar ook de bijbehorende risico’s. Heeft uw vragen? Dan staat ons team klaar om uw vragen te beantwoorden.',
+        text: 'Wij nemen u zorgen gedurende het proces uit handen, en u kunt zich blijven richten op uw core-business. Heeft uw vragen? Dan staan wij en onze partners klaar om uw vragen te beantwoorden.',
         image: 'thumb-up-icon.svg',
         alt: 'icon',
     },
     {
         title: 'Geen terugverdientijd',
-        text: 'Doordat wij de investering doen, heeft u geen terugverdientijd en profiteert u direct van de plaatsing van onze zonnepanelen via dakhuur.',
+        text: 'In het geval dat u kiest voor dakhuur heeft u geen terugverdientijd en profiteert u direct van de plaatsing van de zonnepanelen.',
         image: 'time-icon.svg',
         alt: 'icon',
     },
     {
         title: 'Gezamenlijk belang',
-        text: 'Wij maken gebruik van de beste kwaliteit materialen en zonnepanelen. Wij halen het meeste rendement uit uw dak.',
+        text: 'Ons netwerk bestaat enkel uit experts die gebruik maken van de beste kwaliteit materialen.',
         image: 'hands-icon.svg',
-        alt: 'icon',
-    },
-    {
-        title: 'Inkomsten dakhuur',
-        text: 'Zodra de zonnepanelen zijn aangesloten op het elektriciteitsnet, ontvangt u het maandelijkse huurbedrag en start u met verdienen.',
-        image: 'roof-icon.svg',
         alt: 'icon',
     },
 ];
@@ -70,8 +58,8 @@ export default Controller.extend({
     actions: {
         slideChanged(index) {
             // index seems bugged, correct manually
-            index = index === -1 ? 9 : index;
-            index = index === 10 ? 0 : index;
+            index = index === -1 ? this.slides.length - 1 : index;
+            index = index === this.slides.length ? 0 : index;
             this.set('carouselIndex', index + 1);
         }
     }
